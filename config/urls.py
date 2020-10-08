effa6450a876c73da6fb2dcc5cbe8ceefc898da8
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-10-08 09:48:14 +0900

    add - drf-yasg
    n/a

--- config/urls.py
+++ config/urls.py
@@ -13,3 +13,21 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+# api document
+if settings.DEBUG:
+    from drf_yasg.views import get_schema_view
+    from django.urls import re_path
+    from drf_yasg import openapi
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="API",
+            default_version='v1',
+        ),
+    )
+
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    ]
