3e04780bcb2275d5f0be8b1218128b26d051d9a5
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-10-14 14:19:58 +0900

    add - board detail query
    n/a

--- boards/schema.py
+++ boards/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Board
@@ -6,4 +7,5 @@ from .types.board_types import BoardType
 
 
 class BoardsQueries(graphene.ObjectType):
+    board = relay.Node.Field(BoardType)
     boards = DjangoFilterConnectionField(BoardType)
