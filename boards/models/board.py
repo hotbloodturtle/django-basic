cd0caaf3809c9e3a839597b051487837affc6c92
HaeSeok Kang <hotbloodturtle@gmail.com>
2020-11-16 12:46:39 +0900

    update - board model image field required false

--- boards/models/board.py
+++ boards/models/board.py
@@ -7,5 +7,5 @@ class Board(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
-    photo = models.ImageField(blank=True, upload_to="boards/%Y/%m/%d")
+    photo = models.ImageField(blank=True, upload_to="boards/%Y/%m/%d", null=True)
     created_at = models.DateTimeField(auto_now_add=True)
